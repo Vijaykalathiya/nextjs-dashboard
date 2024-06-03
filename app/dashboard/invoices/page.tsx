89c710b0514b60094bd7d76cc2b2bba0f9c5cbd1
Vijay Kalathiya <vijaykalathiya8@gmail.com>
2024-06-03 15:16:53 +0100

    pagination added on the client side with server query

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -2,8 +2,18 @@ import { lusitana } from "@/app/ui/fonts"
 import { CreateInvoice } from "@/app/ui/invoices/buttons"
 import Pagination from "@/app/ui/invoices/pagination"
 import Search from "@/app/ui/search"
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons"
+import { Suspense } from "react"
+import InvoicesTable from "@/app/ui/invoices/table"
+import { fetchInvoicesPages } from "@/app/lib/data"
 
-const page = () => {
+const page = async ({searchParams}: {searchParams?: {
+  query?: string,
+  page?: number,
+}}) => {
+  const query = searchParams?.query || '';
+  const currentPage = searchParams?.page || 1;
+  const invoicePages = await fetchInvoicesPages(query);
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-center">
@@ -13,11 +23,11 @@ const page = () => {
         <Search placeholder="Search Invoices..." />
         <CreateInvoice />
       </div>
-      {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
-        <Table query={query} currentPage={currentPage} />
-      </Suspense> */}
+       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <InvoicesTable query={query} currentPage={currentPage} />
+      </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={1}/> */}
+        <Pagination totalPages={invoicePages}/>
       </div>
     </div>
   )
