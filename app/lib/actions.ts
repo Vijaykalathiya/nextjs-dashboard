a3241fa8964aa81d2478b364e12498186d9c2567
Vijay Kalathiya <vijaykalathiya8@gmail.com>
2024-06-07 13:00:27 +0100

    edit invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -29,3 +29,18 @@ export async function createInvoice(formData: FormData) {
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
+
+const UpdateInvoice = formSchema.omit({id: true, date: true});
+export async function updateInvoice(id: string, formData: FormData) {
+    const {customerId, amount, status} = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    // const rawFormData = Object.fromEntries(formData.entries())
+    const amountInCents = amount * 100;
+    await sql`UPDATE invoices SET customer_id=${customerId}, amount=${amountInCents}, status=${status} WHERE id=${id}`;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
